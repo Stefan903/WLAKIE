bb7bd1ef0db55e8e10851eb0a97a31352d13a319
Stefan903 <arnick174@gmail.com>
2025-02-27 14:46:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -146,7 +146,6 @@ if st.button("Salir de la aplicación"):
     st.session_state.clear()
     st.experimental_rerun()
     
-    # Iniciar el servidor
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
+# Ejemplo de un botón
+if st.button("Haz clic aquí"):
+    st.write("¡Funciona!")
